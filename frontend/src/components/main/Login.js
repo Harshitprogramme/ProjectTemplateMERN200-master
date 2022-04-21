4b95b594ebf1c345878e15ef9d3a9544f4dce0d9
Harshitprogramme <harshitpandey1516>
2022-04-21 14:46:16 +0530

    ..

---
+++ frontend/src/components/main/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+const login = () => {
+  return (
+   <div>
+   
+   <div class="mycard" >
+   <div class="col-md-4 col-sm-6 mx-auto my-auto">
+        <h3 class="sign">LOGIN HERE </h3>
+        <h2> <input  class="myinput" placeholder="Email" type="Email" /></h2>
+        <h2> <input class="myinput" placeholder="Password" type="Password " /></h2> 
+        <button className="btn btn-danger w-100 mt-5">Submit</button>
+        
+    </div>
+
+  </div>
+  </div>
+  
+  )};
+
+export default login;
